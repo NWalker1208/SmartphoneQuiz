70bc8ecb78439d0dc1af125e23d118176e79914c
NWalker1208 <nathanwalker1208@gmail.com>
2018-04-09 21:41:07 -0700

    Added unused XML request to test access to XML file

--- quiz.js
+++ quiz.js
@@ -2,37 +2,57 @@ var questionTemplate = document.getElementById('question-template').content;
 var choiceTemplate = document.getElementById('choice-template').content;
 var submitButton = document.getElementById('submit');
 
-var questions = [
-	{'text': 'Test', 'category': 'features', 'choices': [
-		{'text': 'A', 'value': 0},
-		{'text': 'B', 'value': 1},
-		{'text': 'C', 'value': 2}]
-	},
-	{'text': 'Test Again', 'category': 'features', 'choices': [
-		{'text': '1', 'value': 0},
-		{'text': '2', 'value': 1}]
-	}
-];
-
-for (var i = 0; i < questions.length; i++)
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function()
 {
-	var question = questions[i];
-	var questionElement = questionTemplate.cloneNode(true);
-	console.log(questionElement);
-	questionElement.querySelector('.question').setAttribute("data-category", question.category);
-	questionElement.querySelector('.question-header>h2').innerText = "Question " + (i + 1);
-	questionElement.querySelector('.question-text').innerText = question.text;
-	
-	console.log(questionElement);
-	
-	for (var j = 0; j < question.choices.length; j++)
+	if (this.readyState == 4 && this.status == 200)
 	{
-		var choice = question.choices[j];
-		var choiceElement = choiceTemplate.cloneNode(true);
-		choiceElement.querySelector('.choice').setAttribute("data-value", choice.value);
-		choiceElement.querySelector('.choice>p').innerText = choice.text;
-		questionElement.querySelector('.question').appendChild(choiceElement);
+		var questions = convertXMLQuestionsToObjects(this.responseXML);
+		addQuestionsToQuiz(questions);
 	}
-	
-	submitButton.parentNode.insertBefore(questionElement, submitButton);
+};
+xmlhttp.open("GET", "questions.xml", true);
+xmlhttp.send();
+
+function convertXMLQuestionsToObjects(xml)
+{
+	var temp_questions = [
+		{'text': 'Test', 'category': 'features', 'choices': [
+			{'text': 'A', 'value': 0},
+			{'text': 'B', 'value': 1},
+			{'text': 'C', 'value': 2}]
+		},
+		{'text': 'Test Again', 'category': 'features', 'choices': [
+			{'text': '1', 'value': 0},
+			{'text': '2', 'value': 1}]
+		}
+	];
+
+	return temp_questions;
+}
+
+function addQuestionsToQuiz(questions)
+{
+	for (var i = 0; i < questions.length; i++)
+	{
+		var question = questions[i];
+		var questionElement = questionTemplate.cloneNode(true);
+		console.log(questionElement);
+		questionElement.querySelector('.question').setAttribute("data-category", question.category);
+		questionElement.querySelector('.question-header>h2').innerText = "Question " + (i + 1);
+		questionElement.querySelector('.question-text').innerText = question.text;
+		
+		console.log(questionElement);
+		
+		for (var j = 0; j < question.choices.length; j++)
+		{
+			var choice = question.choices[j];
+			var choiceElement = choiceTemplate.cloneNode(true);
+			choiceElement.querySelector('.choice').setAttribute("data-value", choice.value);
+			choiceElement.querySelector('.choice>p').innerText = choice.text;
+			questionElement.querySelector('.question').appendChild(choiceElement);
+		}
+		
+		submitButton.parentNode.insertBefore(questionElement, submitButton);
+	}
 }
